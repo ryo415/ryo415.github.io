20e2c074fe44c2d7010111039c7dbac621da1aed
ryo <ryo.kikuchi.721@gmail.com>
2026-02-22 19:10:37 +0900

    Refactor pages

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const year = document.getElementById("year");
+  if (year) {
+    year.textContent = String(new Date().getFullYear());
+  }
+});
